716907652068fba3702c3c54580a75c1085401df
Denise <yu.denise.d@gmail.com>
2014-12-10 17:11:46 +0000

    heroku images

--- app/models/post.rb
+++ app/models/post.rb
@@ -5,6 +5,4 @@ class Post < ActiveRecord::Base
   has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.jpg"
   validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
 
-  
-
 end
